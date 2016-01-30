426ac83bae35b8cadb4023ba2000218bbd32dc75
Nolan <declercq.n@live.fr>
2016-01-30 18:21:24 +0100

    recommit

---
+++ Assets/Move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+    private float maxSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(hor * maxSpeed, ver * maxSpeed);
+	}
+}
