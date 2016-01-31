e2abdb87406fa6f0e02459fc5d9bd0338a8d0510
Nolan <declercq.n@live.fr>
2016-01-31 15:24:59 +0100

    [Update] Random Spawn (add the locations to the list !)

--- Assets/Scripts/Circle.cs
+++ Assets/Scripts/Circle.cs
@@ -6,14 +6,59 @@ using System.Collections;
 public class Circle : MonoBehaviour {
     public bool ritualReady = false;
     public int fail;
+    public int items = 0;
     public int[] cult = new int[3] { 0, 0, 0 };
+
+    public ArrayList inGame = new ArrayList();
+    public ArrayList itemList = new ArrayList();
+    public Vector3[] positions = { new Vector3(1, 2, 3), new Vector3( 1, 5, 0) }; //Add all the positions
 
     static public int book = 0;
 
     // Use this for initialization
     void Start () {
-	    //Alea-Spawn
+
+        itemList.Add((GameObject)Resources.Load("Cat"));
+        itemList.Add((GameObject)Resources.Load("Chose Informe"));
+        itemList.Add((GameObject)Resources.Load("Citron Vert"));
+        itemList.Add((GameObject)Resources.Load("Clous"));
+        itemList.Add((GameObject)Resources.Load("Corbeau"));
+        itemList.Add((GameObject)Resources.Load("Coussin Chaos"));
+        itemList.Add((GameObject)Resources.Load("Coussin Cthulu"));
+        itemList.Add((GameObject)Resources.Load("Coussin Etoile"));
+        itemList.Add((GameObject)Resources.Load("Crane Bleu"));
+        itemList.Add((GameObject)Resources.Load("Crane Rouge"));
+        itemList.Add((GameObject)Resources.Load("Crane Vert"));
+        itemList.Add((GameObject)Resources.Load("Cristal Bleu"));
+        itemList.Add((GameObject)Resources.Load("Cristal Rouge"));
+        itemList.Add((GameObject)Resources.Load("Cristal Vert"));
+        itemList.Add((GameObject)Resources.Load("Idole Chaos"));
+        itemList.Add((GameObject)Resources.Load("Idole Cthulu"));
+        itemList.Add((GameObject)Resources.Load("Idole Satan"));
+        itemList.Add((GameObject)Resources.Load("Nyarly"));
+        itemList.Add((GameObject)Resources.Load("Peluche Zeentch"));
+        itemList.Add((GameObject)Resources.Load("Poisson"));
+        itemList.Add((GameObject)Resources.Load("Poupee"));
+        itemList.Add((GameObject)Resources.Load("Sel"));
+        itemList.Add((GameObject)Resources.Load("Tentacle Plastic"));
+        itemList.Add((GameObject)Resources.Load("Truc Serpent"));
+        //Alea-Spawn
+        Spawn();
+
 	}
+
+    void Spawn()
+    {
+        System.Random rand = new System.Random();
+        int number = rand.Next(3, 6);
+
+        for (int i = 0; i < number; i++)
+        {
+            items++;
+            inGame.Add(Instantiate((GameObject)itemList[rand.Next(0, 23)]));
+            ((GameObject)inGame[items - 1]).transform.position = positions[rand.Next(0, 1)];
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -48,6 +93,10 @@ public class Circle : MonoBehaviour {
                 }
             }
 
+            inGame.Remove(i1);
+            inGame.Remove(i2);
+            items -= 2;
+
             float r, v, b;
 
             if (cult[0] + cult[1] + cult[2] != 0)
@@ -77,7 +126,7 @@ public class Circle : MonoBehaviour {
             }
             else
             {
-                Start();
+                Spawn();
             }
         }
 	}
@@ -87,24 +136,4 @@ public class Circle : MonoBehaviour {
 
     void Win(int cult)
     { }
-    /*
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.tag == "Item")
-        {
-            items[collidedWith] = other.gameObject;
-            collidedWith++;
-
-            ritualReady = (collidedWith == 2) ? true : false;
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D other) {
-        if (other.gameObject.tag == "Item")
-        {
-            collidedWith--;
-            items[collidedWith] = null;
-
-            ritualReady = (collidedWith == 2) ? true : false;
-        }
-    }*/
 }
