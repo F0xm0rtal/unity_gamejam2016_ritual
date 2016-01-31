6bd13021eba7d9a9c3e9575833005c979786202f
unknown <gamejam@D-34-29.bellecour.local>
2016-01-31 05:51:11 +0100

    [New] Script managing wrongly overlapping sprites

---
+++ Assets/Scripts/FurnitureLayerManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FurnitureLayerManager : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<SpriteRenderer>().sortingOrder = GetComponent<SpriteRenderer>().sortingOrder - 1;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<SpriteRenderer>().sortingOrder += 1;
+        }
+    }
+}
