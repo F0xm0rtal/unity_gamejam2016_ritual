0079dfe06e6beeca9febc927f1ccfe3489c9a0ad
unknown <gamejam@D-34-29.bellecour.local>
2016-01-31 02:11:21 +0100

    [FIX] No more twiiiiiiiiiist / Sprite do flip right-left

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -1,19 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Move : MonoBehaviour {
-    private float maxSpeed = 10f;
+public class Move : MonoBehaviour
+{
+    public float maxSpeed = 1f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().freezeRotation = true;
+    }
+
+	void FixedUpdate ()
+    {
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
 
         GetComponent<Rigidbody2D>().velocity = new Vector2(hor * maxSpeed, ver * maxSpeed);
+
+        if (GetComponent<Rigidbody2D>().velocity.x > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (GetComponent<Rigidbody2D>().velocity.x < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
 	}
 }
