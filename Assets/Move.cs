6f1369822075e5e2f485f71266f0325de886e21f
unknown <gamejam@D-34-29.bellecour.local>
2016-01-31 03:43:04 +0100

    [Fix] Player sprite animation

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class Move : MonoBehaviour
 {
-    public float maxSpeed = 1f;
+    public float speed = 1.0f;
+    public float scaleFactor = 1.0f;
 
     void Start()
     {
         GetComponent<Rigidbody2D>().freezeRotation = true;
+    }
+
+    void Update()
+    {
+        transform.localScale = new Vector3(1 + transform.position.y * -scaleFactor, 1 + transform.position.y * -scaleFactor, 0.0f);
     }
 
 	void FixedUpdate ()
@@ -15,15 +21,25 @@ public class Move : MonoBehaviour
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
 
-        GetComponent<Rigidbody2D>().velocity = new Vector2(hor * maxSpeed, ver * maxSpeed);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(hor * speed, ver * speed);
 
         if (GetComponent<Rigidbody2D>().velocity.x > 0)
         {
             GetComponent<SpriteRenderer>().flipX = false;
         }
+
         if (GetComponent<Rigidbody2D>().velocity.x < 0)
         {
             GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (GetComponent<Rigidbody2D>().IsAwake())
+        {
+            GetComponent<Animator>().SetBool("Walk", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Walk", false);
         }
 	}
 }
